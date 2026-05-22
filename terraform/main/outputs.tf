4f14ce9ea0f3af3e7541f9588887de92d8d92cef
Vansh Khanna <vanshkhanna17@gmail.com>
2026-05-22 11:06:17 +0530

    feat: Add awscli and bootstrap improvements

--- terraform/main/outputs.tf
+++ terraform/main/outputs.tf
@@ -5,3 +5,7 @@ output "ec2_public_ip" {
 output "ec2_public_dns" {
   value = aws_instance.app_server.public_dns
 }
+
+output "ecr_repository_url" {
+  value = aws_ecr_repository.app_repo.repository_url
+}
--- terraform/main/userdata.sh
+++ terraform/main/userdata.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+set -e
+
 apt update -y
 
-apt install -y docker.io git
+apt install -y docker.io git awscli
 
 systemctl enable docker
 systemctl start docker
 
-curl -SL https://github.com/docker/compose/releases/download/v5.1.3/docker-compose-linux-aarch64 \
+curl -SL https://github.com/docker/compose/releases/download/v2.24.6/docker-compose-linux-x86_64 \
 -o /usr/local/bin/docker-compose
 
 chmod +x /usr/local/bin/docker-compose
+
+usermod -aG docker ubuntu
